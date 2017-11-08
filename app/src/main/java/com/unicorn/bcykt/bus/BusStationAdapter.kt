f2d7670963550095b2d596445330646ff9a79b18
ivolianer <ivolianer2014@163.com>
2017-11-08 12:59:09 +0800

    .

--- app/src/main/java/com/unicorn/bcykt/bus/BusStationAdapter.kt
+++ app/src/main/java/com/unicorn/bcykt/bus/BusStationAdapter.kt
@@ -5,9 +5,19 @@ import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.unicorn.bcykt.R
 
-class BusStationAdapter: BaseQuickAdapter<BusStationItem, BaseViewHolder>(R.layout.item_bus_station) {
+class BusStationAdapter : BaseQuickAdapter<BusStationItem, BaseViewHolder>(R.layout.item_bus_station) {
 
-    override fun convert(helper: BaseViewHolder, item: BusStationItem) {}
+    override fun convert(helper: BaseViewHolder, item: BusStationItem) {
+        item.busStationName.let { helper.setText(R.id.tvName, it) }
+        item.busLineItems
+                .map { it.busLineName }
+                .map { it.substring(0, it.indexOf('(')) }
+                .distinct()
+                .joinToString(",")
+                .let {
+                    helper.setText(R.id.tvLines, it)
+                }
+    }
 
 }
 
